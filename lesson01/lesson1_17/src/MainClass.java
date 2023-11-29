f8bfe534b8250d87493ee63bdd76b0cda871184f
romanowskaelena <148208289+romanowskaelena@users.noreply.github.com>
2023-11-29 20:21:59 +0100

    Add files via upload

---
+++ lesson01/lesson1_17/src/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+    public static void main(String[] args) {
+    System.out.println("Hello world!");
+    }
+}
+
+//классы -с большой буквы Класс.подкласс.метод("информация"")//
